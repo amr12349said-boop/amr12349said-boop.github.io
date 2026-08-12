7c7441410c97cd608f0670d556de6714360ca62a
amr12349said-boop <267388758+amr12349said-boop@users.noreply.github.com>
2026-08-12 03:57:52 +0200

    deploy: صورة اللوجو + بطاقات بالصور + تثبيت كتطبيق PWA

---
+++ sw.js
@@ -0,0 +1,44 @@
+const CACHE = "mora3-v1";
+const CORE = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./photo.jpg"
+];
+
+self.addEventListener("install", e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(CORE)).then(() => self.skipWaiting()));
+});
+
+self.addEventListener("activate", e => {
+  e.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch", e => {
+  if (e.request.method !== "GET") return;
+  const url = new URL(e.request.url);
+  if (url.origin !== location.origin) return;
+  if (url.pathname.startsWith("/img/")) {
+    e.respondWith(
+      caches.match(e.request).then(hit => hit || fetch(e.request).then(res => {
+        const copy = res.clone();
+        caches.open(CACHE).then(c => c.put(e.request, copy));
+        return res;
+      }))
+    );
+    return;
+  }
+  e.respondWith(
+    caches.match(e.request).then(hit =>
+      hit || fetch(e.request).then(res => {
+        const copy = res.clone();
+        caches.open(CACHE).then(c => c.put(e.request, copy));
+        return res;
+      })
+    )
+  );
+});
